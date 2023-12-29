f4cb7643c563110e6cbf6345890edae4d385e53f
JethroNatividad <natividad.jet@gmail.com>
2023-12-29 15:20:45 +0800

    implement round decimal

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@
 // Process: total the prices, tax, and grand total.
 // Output: Subtotal, Tax, Total.
 
+fn round_decimal(number: f64) -> f64 {
+    (number * 1000.0).round() / 1000.0
+}
+
 fn calculate_tax(subtotal: f64) -> f64 {
-    const TAX_RATE: i64 = 0.055; // 5.5%
+    const TAX_RATE: f64 = 0.055; // 5.5%
     subtotal * TAX_RATE
 }
 
@@ -20,11 +24,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_round_decimal() {
+        assert_eq!(round_decimal(27.870912), 27.871);
+        assert_eq!(round_decimal(2.3225), 2.323);
+        assert_eq!(round_decimal(4.895), 4.895);
+    }
+
+    #[test]
     fn test_calculate_tax(){
-        assert_eq!(calculate_tax(64), 3.52)
-        assert_eq!(calculate_tax(55), 3.03)
-        assert_eq!(calculate_tax(120), 6.6)
-        assert_eq!(calculate_tax(89), 4.9)
+        assert_eq!(calculate_tax(64.0), 3.52);
+        assert_eq!(calculate_tax(55.0), 3.025);
+        assert_eq!(calculate_tax(120.0), 6.6);
+        assert_eq!(calculate_tax(89.0), 4.895);
     }
 }
 
